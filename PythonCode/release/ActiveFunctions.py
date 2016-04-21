c5310ed5068ef2858c7af307d4ae9c6b0d37e45a
Chromiumikx <studyfori@126.com>
2016-04-21 22:32:32 +0800

    当前即最终，取消最终版

--- PythonCode/release/ActiveFunctions.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-
-#非线性映射函数，到0~1的范围；及其导数，当deriv为True时
-def nonlin(x,deriv=False):
-    y=np.tanh(x)
-    if deriv:
-        return (1-y**2)
-    return y
--- PythonCode/release/GR_Service.py
+++
@@ -1,3 +0,0 @@
-##________________________Gesture Recognition Services Component_____________________________
-##主要代码从threading_test_1移植过来
-# -*- coding: UTF8 -*-
