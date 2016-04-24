ef13984fe39a0f6c4e881fdf02d20bcbed8bcba4
Chromiumikx <studyfori@126.com>
2016-04-24 17:22:44 +0800

    项目答辩完成，将长时间不更新

--- PythonCode/Demo - PPT Next Page/PPTNextPage.py
+++ PythonCode/Demo - PPT Next Page/PPTNextPage.py
@@ -14,12 +14,12 @@ try:
         Num=""
         Num=myclient.getGesture()
         if Num == "4":
-            win32api.keybd_event(32,0,0,0)  #空格键位码是32
-            win32api.keybd_event(32,0,win32con.KEYEVENTF_KEYUP,0)
+            win32api.keybd_event(VK_CODE["right_arrow"],0,0,0)  #空格键位码是32
+            win32api.keybd_event(VK_CODE["right_arrow"],0,win32con.KEYEVENTF_KEYUP,0)
 
         if Num == "3":
-            win32api.keybd_event(38,0,0,0)  #向上箭头键位码是38
-            win32api.keybd_event(VK_CODE["up_arrow"],0,win32con.KEYEVENTF_KEYUP,0)
+            win32api.keybd_event(VK_CODE["left_arrow"],0,0,0)  #向上箭头键位码是38
+            win32api.keybd_event(VK_CODE["left_arrow"],0,win32con.KEYEVENTF_KEYUP,0)
 
         if Num == "2":
             win32api.keybd_event(VK_CODE["F5"],0,0,0)  #空格键位码是32
